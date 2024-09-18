13e183002d1fe6b64e447d67b18bd47e25bdfa4e
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-09-18 15:26:41 +0530

    creting the home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import Navbar from "../components/Navbar";
 const HomePage = () => {
   return (
-  )
-}
+    <>
+      <div className="h-[100vh] w-[100vw] bg-black">
+        <Navbar />
+        <div className="h-[93vh] w-[100vw] bg-green-500"></div>
+      </div>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
