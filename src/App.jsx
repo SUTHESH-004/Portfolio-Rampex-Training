b7dceed3f17c155cdfd9b7a95593060c5d65ff4a
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-09-18 14:50:16 +0530

    implemented the basic routes for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { Routes, Route, Router } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import { ProjectsPage } from "./pages/ProjectsPage";
+import ContactPage from "./pages/ContactPage";
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/projects" element={<ProjectsPage />}></Route>
-          <Route path="/contact" element={<ContactPage/>}></Route>
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/projects" element={<ProjectsPage />}></Route>
+        <Route path="/contact" element={<ContactPage />}></Route>
+      </Routes>
       {/* <div className="h-[100vh] w-[100vw] bg-black"></div> */}
     </>
   );
