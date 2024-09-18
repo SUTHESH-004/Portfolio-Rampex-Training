be3a64ec9d14d0c0b4f4d135b57c24270a0be1a0
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-09-18 16:15:35 +0530

    structuring the home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,35 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { User } from "lucide-react";
 const Navbar = () => {
   return (
     <div className="h-[7vh] w-[100vw]  bg-blue-700 flex flex-row">
       <div className="h-[100%] w-[30vw] bg-pink-500 flex">
-        <div className="flex-[3] bg-yellow-500 flex items-center justify-center">
-          Suthesh Pravin{" "}
+        <div className="flex-[5] bg-yellow-500 flex items-center justify-center">
+          SUTHESH PRAVIN V{" "}
         </div>
       </div>
       <div className="h-[100%] w-[70vw] bg-red-600 flex ">
-        <div className="flex-[1] bg-yellow-50 flex items-center justify-center">
-          Home
-        </div>
-        <div className="flex-[1] bg-white/65 flex items-center justify-center">
-          Projects
-        </div>
-        <div className="flex-[1] bg-pink-500 flex items-center justify-center">
-          Contact
+        <NavLink
+          to="/"
+          className="flex-[3] bg-yellow-50 flex items-center justify-center"
+        >
+          HOME
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className="flex-[3] bg-white/65 flex items-center justify-center"
+        >
+          PROJECTS
+        </NavLink>
+        <NavLink
+          to="contact"
+          className="flex-[3] bg-pink-500 flex items-center justify-center"
+        >
+          CONTACT
+        </NavLink>
+        <div className="flex-[1] bg-blue-500 flex items-center justify-center">
+          <User />
         </div>
       </div>
     </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,10 @@ const HomePage = () => {
     <>
       <div className="h-[100vh] w-[100vw] bg-black">
         <Navbar />
-        <div className="h-[93vh] w-[100vw] bg-green-500"></div>
+        <div className=" flex flex-row h-[93vh] w-[100vw] bg-green-500 gap-4">
+          <div className="h-[80%] my-auto w-[40%]  bg-blue-500"></div>
+          <div className="h-[80%] my-auto w-[60%]  bg-black"></div>
+        </div>
       </div>
     </>
   );
